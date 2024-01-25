8b4dbfe4f8c8fcbcf24b6a0e4ab8c46d286c9d4c
Ureed-Hussain <ureedhussain214@gmail.com>
2024-01-25 05:54:43 -0800

    req edit

--- app.py
+++ app.py
@@ -16,26 +16,29 @@ slt.header('sepal length (cm)')
 
 a = slt.text_input("Enter number ")
 
+a = int(a)
+
 slt.header('sepal width (cm)')
 
 b = slt.text_input("Enter num")
-
+b = int(b)
 slt.header('petal length (cm)')
 
 c = slt.text_input("Enter number")
+c=int(c)
 
 slt.header('petal width (cm)')
 
 d = slt.text_input("Enter Num")
-
+d=int(d)
 
 if slt.button("predict"):
     result =model.predict([[a,b,c,d]])
 
-    if result == ["0"]:
+    if result == [0]:
         slt.header('setosa')
         print("setosa")
-    elif result == ["1"]:
+    elif result == [1]:
         slt.header('versicolor')
         print("versicolor")
     else:
